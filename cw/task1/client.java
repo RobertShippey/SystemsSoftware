4ef7c823d4c9e30fb76528b30cd9b84013676fc1
Robert Shippey <robert@robertshippey.net>
2012-03-09 22:02:22 +0000

    Rewrote with ObjectStreams
    Boom!

--- cw/task1/client.java
+++ cw/task1/client.java
@@ -2,28 +2,45 @@ import java.net.*;
 import java.io.*;
 
 public class client {
-
-public static void main(String[] args) throws IOException{
-
-	BufferedReader cmd = new BufferedReader(new InputStreamReader(System.in));
-	System.out.println("Enter the IP Address of the server");
-	String ip = cmd.readLine();
-    //172.19.115.213
-
-	Socket soc = new Socket(ip, 2000);
     
-    PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
-    BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-
-
-    System.out.println("Enter message to send to server: ");
-    out.println(cmd.readLine());
-
-    String m = in.readLine();
-    System.out.println("Server send back: ");
-    System.out.println(m);
-	
-
-}
-
+    public static void main(String[] args) {
+        
+        try{
+            BufferedReader cmd = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter the IP Address of the server");
+            String ip = cmd.readLine();
+            
+            Socket soc = new Socket(ip, 2000);
+            
+            ObjectOutputStream out = new ObjectOutputStream(soc.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream(soc.getInputStream());
+            
+            
+            String m;
+            System.out.println("Enter message to send to server: ");
+            m = cmd.readLine();
+            out.writeObject(m);
+            
+            m = (String)in.readObject();
+            System.out.println("Server send back: ");
+            System.out.println(m);
+            System.out.println("Done!");
+        } catch (UnknownHostException uhe) {
+            System.err.println(uhe.getMessage());
+            System.err.println("Could not connect to the server.");
+            System.exit(0);
+            
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            System.err.println("Communication could not be established.");
+            System.exit(0);
+        } catch (ClassNotFoundException cnf) {
+            System.err.println(cnf.getMessage());
+            System.err.println("The server send something weird back. Appologies.");
+            System.exit(0);
+        }
+        
+        
+    }
+    
 }
--- cw/task1/server.java
+++ cw/task1/server.java
@@ -3,21 +3,30 @@ import java.io.*;
 
 public class server {
     
-    public static void main (String[] args) throws IOException {
+    public static void main (String[] args) {
+        try {
         BufferedReader cmd = new BufferedReader(new InputStreamReader(System.in));
         ServerSocket serv = new ServerSocket(2000);
        	System.out.println("Waiting...");
         Socket client = serv.accept();
         
-        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
+        ObjectInputStream in = new ObjectInputStream(client.getInputStream());
         
-        String m = in.readLine();
+        String m = (String) in.readObject();
         System.out.println("Client sent: ");
         System.out.println(m);
         
         System.out.println("Enter server's response: ");
-        out.println(cmd.readLine());
+        m = cmd.readLine();
+        out.writeObject(m);
         System.out.println("Done!");
+        } catch (IOException e){
+            System.err.println(e.getMessage());
+            System.exit(0);
+        } catch (ClassNotFoundException cnf) {
+            System.err.println(cnf.getMessage());
+            System.exit(0);
+        }
     }
 }
