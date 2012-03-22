14226f650db0e2f600d9ef0cf5ba4d89d737ae59
Robert Shippey <robert@robertshippey.net>
2012-03-22 21:17:45 +0000

    last min fixes

--- cw/task1/client.java
+++ cw/task1/client.java
@@ -1,32 +1,55 @@
-import java.net.*;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 
 public class client {
-    
-    public static void main(String[] args) throws Exception {
-        
-                      BufferedReader cmd = new BufferedReader(new InputStreamReader(System.in));
-                      System.out.println("Enter the IP Address of the server");
-                      String ip = cmd.readLine();
-        InetAddress serverIP = InetAddress.getByName(ip);
+
+    public static void main(String[] args) {
+
+        BufferedReader cmd = new BufferedReader(new InputStreamReader(System.in));
+        InetAddress serverIP = null;
         int serverPort = 2000;
-        
+
+        System.out.println("Enter the IP Address of the server");
+
+        try {
+            String ip = cmd.readLine();
+            serverIP = InetAddress.getByName(ip);
+        } catch (IOException ioe) {
+            System.err.println(ioe.getMessage());
+            System.err.println("Could not reach the specified IP address");
+            System.exit(0);
+        }
+
         byte[] Nnumber = "N0305434".getBytes();
-        
-        DatagramSocket serverSocket = new DatagramSocket();
-        DatagramPacket sendingPacket = new DatagramPacket(Nnumber, Nnumber.length, serverIP, serverPort);
-        serverSocket.send(sendingPacket);
-        
-        byte[] buffer = new byte[50];
-        DatagramPacket receivingPacket = new DatagramPacket(buffer, buffer.length);
-        serverSocket.receive(receivingPacket);
-        
-        String text = new String(receivingPacket.getData(), 0, receivingPacket.getLength());
-        
-        System.out.println(text);
-        
-              
-        
+
+        try {
+            DatagramSocket serverSocket = new DatagramSocket();
+            DatagramPacket sendingPacket = new DatagramPacket(Nnumber, Nnumber.length, serverIP, serverPort);
+            serverSocket.send(sendingPacket);
+
+            byte[] buffer = new byte[50];
+            DatagramPacket receivingPacket = new DatagramPacket(buffer, buffer.length);
+            serverSocket.receive(receivingPacket);
+
+            
+            String text = new String(receivingPacket.getData(), 0, receivingPacket.getLength());
+
+            System.out.println(text);
+            
+            serverSocket.close();
+
+        } catch (SocketException se) {
+            System.err.println(se.getMessage());
+            System.exit(0);
+        } catch (IOException ioe) {
+            System.err.println(ioe.getMessage());
+            System.exit(0);
+        }
     }
-    
 }
--- cw/task1/server.java
+++ cw/task1/server.java
@@ -1,25 +1,41 @@
-import java.net.*;
-import java.io.*;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 
 public class server {
-    
-    public static void main (String[] args) throws Exception {
-        
-        DatagramSocket mySocket = new DatagramSocket(2000);
-        
-        byte[] buffer = new byte[50];
-        DatagramPacket recievedPacket = new DatagramPacket(buffer, buffer.length);
-        mySocket.receive(recievedPacket);
-        
-        String text = new String (recievedPacket.getData(), 0, recievedPacket.getLength());
-       System.out.println(text);
-        
-        byte[] myName = "Robert Shippey".getBytes();
-        
-        InetAddress clientIP = recievedPacket.getAddress();
-        int clientPort = recievedPacket.getPort();
-        DatagramPacket sendingPacket = new DatagramPacket(myName, myName.length, clientIP, clientPort);
-        mySocket.send(sendingPacket);
 
+    public static void main(String[] args) {
+
+        try {
+            DatagramSocket mySocket = new DatagramSocket(2000);
+
+            byte[] buffer = new byte[50];
+            DatagramPacket recievedPacket = new DatagramPacket(buffer, buffer.length);
+            mySocket.receive(recievedPacket);
+
+            String text = new String(recievedPacket.getData(), 0, recievedPacket.getLength());
+            System.out.println(text);
+
+            byte[] myName = "Robert Shippey".getBytes();
+
+            InetAddress clientIP = recievedPacket.getAddress();
+            int clientPort = recievedPacket.getPort();
+            DatagramPacket sendingPacket = new DatagramPacket(myName, myName.length, clientIP, clientPort);
+            mySocket.send(sendingPacket);
+            
+            mySocket.close();
+            
+        } catch (SocketException se) {
+            System.err.println(se.getMessage());
+            System.err.println("Could not reserver port 2000");
+            System.exit(0);
+        } catch (IOException ioe) {
+            System.err.println(ioe.getMessage());
+            System.err.println("Could not send/recieve message");
+            System.exit(0);
+        }
     }
 }
